8aa57701f7f2103cc63cbe648bfab76b15af1b83
romanusbett <bettromanus@gmail.com>
2025-09-02 11:55:17 +0300

    add a loader on user login

--- reci-app/src/pages/auth_pages/user_login.tsx
+++ reci-app/src/pages/auth_pages/user_login.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import AuthForm from "../../components/authForm";
 import { toast } from "react-toastify";
+import HashLoader from "react-spinners/HashLoader";
 
 const UserLogin = () => {
+  const [loading, setLoading] = useState(false);
+
   const handleLogin = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
+    setLoading(true);
 
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email");
@@ -19,30 +24,50 @@ const UserLogin = () => {
       if (!res.ok) throw new Error("Invalid login credentials");
 
       const data = await res.json();
-
       localStorage.setItem("token", data.access_token);
 
       window.location.href = "/user-home";
     } catch (err) {
       console.error(err);
       toast.error("Login failed. Please check your credentials.");
+    } finally {
+      setLoading(false); 
     }
   };
 
   return (
-    <AuthForm
-      title="Welcome Back"
-      subtitle="Sign in to your Recipick account"
-      fields={[
-        { label: "Email", type: "email", id: "email", name: "email" },
-        { label: "Password", type: "password", id: "password", name: "password" },
-      ]}
-      buttonText="Login"
-      onSubmit={handleLogin}
-      redirectText="Don't have an account?"
-      redirectHref="/register"
-      redirectBtnText="Register Here"
-    />
+    <>
+      {loading && (
+        <div
+          style={{
+            position: "fixed",
+            inset: 0,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            zIndex: 9999,
+          }}
+        >
+          <HashLoader color="#16A34A" size={80} />
+        </div>
+      )}
+
+      <AuthForm
+        title="Welcome Back"
+        subtitle="Sign in to your Recipick account"
+        fields={[
+          { label: "Email", type: "email", id: "email", name: "email" },
+          { label: "Password", type: "password", id: "password", name: "password" },
+        ]}
+        buttonText={loading ? "Logging in..." : "Login"}
+        onSubmit={handleLogin}
+        redirectText="Don't have an account?"
+        redirectHref="/register"
+        redirectBtnText="Register Here"
+        disabled={loading}
+      />
+    </>
   );
 };
 
